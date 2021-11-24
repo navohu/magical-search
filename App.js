f6beb73ca9b9148c6646d498efb6b9bf19a5cd6f
navohu <nathalievh95@gmail.com>
2021-11-24 16:03:49 +0000

    basic search

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React, { createElement, Component } from 'react';
 import { getAlgoliaResults } from '@algolia/autocomplete-js';
 import algoliasearch from 'algoliasearch';
-import { Autocomplete } from './Autocomplete';
+import Autocomplete from './Autocomplete';
 import { ProductItem } from './ProductItem';
-import './App.css';
+import './style.css';
 
 const appId = 'latency';
 const apiKey = '6be0576ff61c053d5f9a3225e2a90f76';
@@ -12,30 +12,27 @@ const searchClient = algoliasearch(appId, apiKey);
 class App extends Component {
   render() {
     return (
-      <div className="app-container">
-        <h1>React Application</h1>
+      <div className="wrapper">
+        <h1>Magical Search</h1>
+        <h2>Want to see something cool?, start typing</h2>
         <Autocomplete
-          openOnFocus={true}
-          getSources={({ query }) => [
-            {
-              sourceId: 'products',
-              getItems() {
-                return getAlgoliaResults({
-                  searchClient,
-                  queries: [
-                    {
-                      indexName: 'instant_search',
-                      query,
-                    },
-                  ],
-                });
-              },
-              templates: {
-                item({ item, components }) {
-                  return <ProductItem hit={item} components={components} />;
-                },
-              },
-            },
+          suggestions={[
+            'Angular',
+            'Blitzjs',
+            'Gatsby',
+            'Reactjs',
+            'Vuejs',
+            'Svelte',
+            'Nextjs',
+            'Node',
+            'Express',
+            'Sails',
+            'Loopback',
+            'React-router',
+            'Redux',
+            'Flux',
+            'Yarn',
+            'Npm',
           ]}
         />
       </div>
--- Autocomplete.js
+++ Autocomplete.js
@@ -1,28 +1,98 @@
-import { autocomplete } from '@algolia/autocomplete-js';
-import React, { createElement, Fragment, useEffect, useRef } from 'react';
-import { render } from 'react-dom';
+import { useState } from 'react';
+import React, { createElement, Component } from 'react';
 
-export function Autocomplete(props) {
-  const containerRef = useRef(null);
+const AutoComplete = ({ suggestions }) => {
+  const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+  const [activeSuggestionIndex, setActiveSuggestionIndex] = useState(0);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  const [input, setInput] = useState('');
 
-  useEffect(() => {
-    if (!containerRef.current) {
-      return undefined;
+  const onChange = (e) => {
+    const userInput = e.target.value;
+
+    // Filter our suggestions that don't contain the user's input
+    const unLinked = suggestions.filter(
+      (suggestion) =>
+        suggestion.toLowerCase().indexOf(userInput.toLowerCase()) > -1
+    );
+
+    setInput(e.target.value);
+    setFilteredSuggestions(unLinked);
+    setActiveSuggestionIndex(0);
+    setShowSuggestions(true);
+  };
+
+  const onClick = (e) => {
+    setFilteredSuggestions([]);
+    setInput(e.target.innerText);
+    setActiveSuggestionIndex(0);
+    setShowSuggestions(false);
+  };
+
+  const onKeyDown = (e) => {
+    // User pressed the enter key
+    if (e.keyCode === 13) {
+      setInput(filteredSuggestions[activeSuggestionIndex]);
+      setActiveSuggestionIndex(0);
+      setShowSuggestions(false);
     }
+    // User pressed the up arrow
+    else if (e.keyCode === 38) {
+      if (activeSuggestionIndex === 0) {
+        return;
+      }
 
-    const search = autocomplete({
-      container: containerRef.current,
-      renderer: { createElement, Fragment },
-      render({ children }, root) {
-        render(children, root);
-      },
-      ...props,
-    });
+      setActiveSuggestionIndex(activeSuggestionIndex - 1);
+    }
+    // User pressed the down arrow
+    else if (e.keyCode === 40) {
+      if (activeSuggestionIndex - 1 === filteredSuggestions.length) {
+        return;
+      }
 
-    return () => {
-      search.destroy();
-    };
-  }, [props]);
+      setActiveSuggestionIndex(activeSuggestionIndex + 1);
+    }
+  };
 
-  return <div ref={containerRef} />;
-}
+  const SuggestionsListComponent = () => {
+    return filteredSuggestions.length ? (
+      <ul class="suggestions">
+        {filteredSuggestions.map((suggestion, index) => {
+          let className;
+
+          // Flag the active suggestion with a class
+          if (index === activeSuggestionIndex) {
+            className = 'suggestion-active';
+          }
+
+          return (
+            <li className={className} key={suggestion} onClick={onClick}>
+              {suggestion}
+            </li>
+          );
+        })}
+      </ul>
+    ) : (
+      <div class="no-suggestions">
+        <span role="img" aria-label="tear emoji">
+          😪
+        </span>{' '}
+        <em>sorry no suggestions</em>
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <input
+        type="text"
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      {showSuggestions && input && <SuggestionsListComponent />}
+    </>
+  );
+};
+
+export default AutoComplete;
