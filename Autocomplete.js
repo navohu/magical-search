258a3660984984c70852ebd13b3a5f464e113d3c
navohu <nathalievh95@gmail.com>
2021-11-30 17:55:39 +0000

    filter categories

--- Autocomplete.js
+++ Autocomplete.js
@@ -8,7 +8,6 @@ var _ = require('lodash');
 const AutoComplete = ({ animals, categories }) => {
   const [filteredSuggestions, setFilteredSuggestions] = useState(animals);
   const [activeSuggestionIndex, setActiveSuggestionIndex] = useState(0);
-  const [showSuggestions, setShowSuggestions] = useState(false);
   const [input, setInput] = useState('');
 
   const onChange = (e) => {
@@ -25,24 +24,17 @@ const AutoComplete = ({ animals, categories }) => {
     setInput(userInput);
     setFilteredSuggestions(unLinked);
     setActiveSuggestionIndex(0);
-    setShowSuggestions(true);
   };
 
   const onClick = (e) => {
     const userInput = e.target.innerText;
-    const foundElement = animals.map((animal) => {
-      return {
-        category: animal.category,
-        breeds: animal.breeds.filter(
-          (breed) =>
-            breed.name.toLowerCase().indexOf(userInput.toLowerCase()) > -1
-        ),
-      };
-    });
+    const foundElement = animals.filter(
+      (animal) =>
+        animal.name.toLowerCase().indexOf(userInput.toLowerCase()) > -1
+    );
     setFilteredSuggestions(foundElement);
     setInput(userInput);
     setActiveSuggestionIndex(0);
-    setShowSuggestions(false);
   };
 
   const onKeyDown = (e) => {
@@ -50,7 +42,6 @@ const AutoComplete = ({ animals, categories }) => {
     if (e.keyCode === 13) {
       setInput(filteredSuggestions[activeSuggestionIndex].name);
       setActiveSuggestionIndex(0);
-      setShowSuggestions(false);
     }
     // User pressed the up arrow
     else if (e.keyCode === 38) {
@@ -67,15 +58,6 @@ const AutoComplete = ({ animals, categories }) => {
       // }
       setActiveSuggestionIndex(activeSuggestionIndex + 1);
     }
-  };
-
-  const ListOfBreedsByCategory = ({ results, category }) => {
-    const filteredAnimalsByCategory = _.filter(
-      results,
-      (result) => result.category === category.id
-    );
-
-    return <AnimalList results={filteredAnimalsByCategory} />;
   };
 
   const AnimalList = ({ results }) => {
@@ -107,43 +89,6 @@ const AutoComplete = ({ animals, categories }) => {
     );
   };
 
-  const CategoriesSearchResult = ({ animal }) => {
-    const listOfBreeds = (
-      <ul class="breed-list">
-        {animal.breeds.map((breed, index) => {
-          let activeItemClassName;
-
-          // Flag the active suggestion with a class
-          if (index === activeSuggestionIndex) {
-            console.log('Index: ' + index);
-            console.log('Active sugg. index: ' + activeSuggestionIndex);
-            activeItemClassName = 'suggestion-active';
-          }
-          console.log(activeItemClassName);
-          // The breeds
-          return (
-            <li className={activeItemClassName} key={breed} onClick={onClick}>
-              {breed}
-            </li>
-          );
-        })}
-      </ul>
-    );
-
-    return animal.breeds.length ? (
-      // Only include animal category if there are still results from the category
-      <>
-        <li class="animal-category" key={animal.category} onClick={onClick}>
-          {animal.category}
-        </li>
-
-        {listOfBreeds}
-      </>
-    ) : (
-      <>{listOfBreeds}</>
-    );
-  };
-
   const NoResults = () => {
     return (
       <div class="no-suggestions text-center">
@@ -168,11 +113,11 @@ const AutoComplete = ({ animals, categories }) => {
     }
 
     return (
-      <Tabs defaultActiveKey="all" transition={true} className="tablist">
+      <Tabs className="tablist" onSelect={(key, ev) => console.log(ev)}>
         <Tab eventKey="all" title="All">
           <AnimalList results={results} />
         </Tab>
-        {categories.map((category, index) => {
+        {categories.map((category) => {
           const filteredAnimalsByCategory = _.filter(
             results,
             (result) => result.category === category.id
@@ -187,34 +132,6 @@ const AutoComplete = ({ animals, categories }) => {
     );
   };
 
-  // const SuggestionsListComponent = () => {
-  //   if (!input.length) {
-  //     console.log(animals);
-  //     return (
-  //       <ul class="suggestions">
-  //         {animals.map((animal, index) => {
-  //           return (
-  //             // The animal category
-  //             <CategoriesSearchResult animal={animal} />
-  //           );
-  //         })}
-  //       </ul>
-  //     );
-  //   }
-
-  //   return _.filter(filteredSuggestions, (x) => x.breeds.length).length ? (
-  //     <ul class="suggestions">
-  //       {filteredSuggestions.map((animal) => {
-  //         return <CategoriesSearchResult animal={animal} />;
-  //       })}
-  //     </ul>
-  //   ) : (
-  //     <div class="no-suggestions text-center">
-  //       <span role="img">😪</span> <em> sorry no suggestions</em>
-  //     </div>
-  //   );
-  // };
-
   return (
     <>
       <input
@@ -224,12 +141,6 @@ const AutoComplete = ({ animals, categories }) => {
         onKeyDown={onKeyDown}
         value={input}
       />
-      {/* <label class="search" for="inpt_search">
-        <input id="inpt_search" type="text"
-        onChange={onChange}
-        onKeyDown={onKeyDown}
-        value={input}/>
-      </label> */}
       <div class="suggestion-list">{<TabListComponent />}</div>
     </>
   );
